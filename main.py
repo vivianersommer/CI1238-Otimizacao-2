a948af1a4ffe575457027d019e36d378ad0554c0
Viviane da Rosa Sommer <viviane.sommer@hitechnologies.com.br>
2022-03-30 20:14:12 -0300

    Inicio da matriz de restricoes

--- main.py
+++ main.py
@@ -6,21 +6,40 @@ def readInput():
 
     itens = []
     for i in range(0, numero_itens):
-        itens.append(input(""))
+        itens.append(int(input("")))
 
     pares = []
     for i in range(0, numero_pares):
-        par_1 = input("")
-        par_2 = input("")
+        par_1 = int(input(""))
+        par_2 = int(input(""))
         pares.append([par_1, par_2])
 
 
     return numero_itens, numero_pares, capacidade, itens, pares
 
-# def create_matrix(numero_itens, numero_pares, capacidade, itens, pares):
-#
-#     values = []
-#     for
+def create_matrix(numero_itens, numero_pares, capacidade, itens, pares):
+
+    values = [] #matrix
+    temp = 0
+    # [ 1, 1 ,1, 0, 0 ,0 , 0,...]
+    # [ 0, 0, 0, 1 ,1 ,1, 0, ...]
+    for i in range(0, numero_itens): # adicionar uma linha
+        line = []
+
+        for i in range(0, temp):
+            line.append(0)
+
+        for i in range(temp, temp + numero_itens):
+            line.append(1)
+
+        for i in range(temp + numero_itens, (numero_itens * numero_itens) + numero_itens):
+            line.append(0)
+
+        values.append([line])
+        temp += numero_itens
+
+    print(values)
 
 if __name__ == '__main__':
     numero_itens, numero_pares, capacidade, itens, pares = readInput()
+    create_matrix(numero_itens, numero_pares, capacidade, itens, pares)
